7d3b597eafe46395b5717ef75998b8a8a0cff996
jotaquark <jotaquark@gmail.com>
2026-02-04 20:43:44 -0300

    Se Crea el js para incorporar al html

---
+++ datos.js
@@ -0,0 +1,5 @@
+// datos.js
+const CERTIFICADO_DATA = {
+    nombreApellido: "JUAN PÉREZ",// Reemplaza con el nombre real o el que quieras aprender
+    tecnologia: "Git y GitHub"// Reemplaza con la tecnología real o el que quieras aprender
+};
