226c1e42568145c44d9d41871a219dfc9ae82b38
jotaquark <jotaquark@gmail.com>
2026-02-04 21:03:02 -0300

    Se Agrega la linea del scrip para incoporar la parte del .js

--- datos.js
+++ datos.js
@@ -1,5 +1,5 @@
 // datos.js
 const CERTIFICADO_DATA = {
     nombreApellido: "JUAN PÉREZ",// Reemplaza con el nombre real o el que quieras aprender
-    tecnologia: "Git y GitHub"// Reemplaza con la tecnología real o el que quieras aprender
+    tecnologia: "Git y GitHub Master"// Reemplaza con la tecnología real o el que quieras aprender
 };
